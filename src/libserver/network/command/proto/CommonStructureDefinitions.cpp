318b1b84c22640f05f1936a58ddcf48968ca3317
Serkan <14278530+SergeantSerk@users.noreply.github.com>
2025-10-26 11:18:32 +0000

    Found horse dirtiness + sparkles

--- src/libserver/network/command/proto/CommonStructureDefinitions.cpp
+++ src/libserver/network/command/proto/CommonStructureDefinitions.cpp
@@ -445,7 +445,8 @@ void Horse::Write(const Horse& value, SinkStream& stream)
 
   stream.Write(value.mastery);
 
-  stream.Write(value.val16).Write(value.val17);
+  stream.Write(value.val16)
+    .Write(value.visualCleanlinessBitset);
 }
 
 void Horse::Read(Horse& value, SourceStream& stream)
@@ -500,7 +501,7 @@ void Horse::Read(Horse& value, SourceStream& stream)
   stream.Read(value.mastery);
 
   stream.Read(value.val16)
-    .Read(value.val17);
+    .Read(value.visualCleanlinessBitset);
 }
 
 void Guild::Write(const Guild& value, SinkStream& stream)
