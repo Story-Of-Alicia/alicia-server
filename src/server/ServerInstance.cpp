c38c67a78ff18cdbf66286234efe4dbed884dcb5
Heather <hjanggeog9@gmail.com>
2025-10-30 23:48:02 +0900

    Implement mane and tail genetics

--- src/server/ServerInstance.cpp
+++ src/server/ServerInstance.cpp
@@ -54,6 +54,7 @@ ServerInstance::ServerInstance(
   , _genetics(*this)
   , _itemSystem(*this)
   , _breedingMarket(*this)
+  , _genetics(*this)
 {
 }
 
@@ -355,6 +356,11 @@ Genetics& ServerInstance::GetGenetics()
 BreedingMarket& ServerInstance::GetBreedingMarket()
 {
   return _breedingMarket;
+}
+
+Genetics& ServerInstance::GetGenetics()
+{
+  return _genetics;
 }
 
 Config& ServerInstance::GetSettings()
