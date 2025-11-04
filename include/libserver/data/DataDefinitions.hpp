c3463b06a0bd96504f5c5a8bdebc789d176511a6
Heather <hjanggeog9@gmail.com>
2025-11-04 23:01:29 +0900

    Implement breeding combo (consecutive success

--- include/libserver/data/DataDefinitions.hpp
+++ include/libserver/data/DataDefinitions.hpp
@@ -347,13 +347,15 @@ struct Character
     dao::Field<Sets> magic{};
   } skills{};
 
-    dao::Field<std::vector<Uid>> dailyQuests{};
+  dao::Field<std::vector<Uid>> dailyQuests{};
   struct Mailbox
   {
     dao::Field<bool> hasNewMail{false};
     dao::Field<std::vector<Uid>> inbox{};
     dao::Field<std::vector<Uid>> sent{};
   } mailbox{};
+
+  dao::Field<uint32_t> breedingMoneySpent{0u};
 };
 
 struct Horse
@@ -401,7 +403,13 @@ struct Horse
   dao::Field<uint32_t> clazzProgress{0u};
   dao::Field<uint32_t> grade{0u};
   dao::Field<uint32_t> growthPoints{0u};
-  dao::Field<uint32_t> timesBreeded{0u};  // Total times used for breeding since it was born
+  
+  struct Breeding
+  {
+    dao::Field<uint32_t> timesBreeded{0u};  // Total times used for breeding since it was born
+    dao::Field<uint8_t> breedingCombo{0u};
+  } breeding{};
+  
   dao::Field<uint8_t> horseType{0u};
   dao::Field<uint8_t> tendency{0u};
   dao::Field<uint8_t> spirit{0u};
