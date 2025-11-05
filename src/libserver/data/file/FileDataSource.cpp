ca48cb40ab22d7873ef6474c5e4dedc59f7304a7
Heather <hjanggeog9@gmail.com>
2025-11-05 21:26:35 +0900

    Implement lineage calculation and inheritance buff

--- src/libserver/data/file/FileDataSource.cpp
+++ src/libserver/data/file/FileDataSource.cpp
@@ -659,6 +659,8 @@ void server::FileDataSource::RetrieveHorse(data::Uid uid, data::Horse& horse)
   {
     horse.ancestors = std::vector<data::Uid>{};
   }
+  
+  horse.lineage = json.value("lineage", uint8_t{1});
 }
 
 void server::FileDataSource::StoreHorse(data::Uid uid, const data::Horse& horse)
@@ -770,6 +772,7 @@ void server::FileDataSource::StoreHorse(data::Uid uid, const data::Horse& horse)
   json["mountInfo"] = mountInfo;
 
   json["ancestors"] = horse.ancestors();
+  json["lineage"] = horse.lineage();
   dataFile << json.dump(2);
 }
 
