77d30b8c260a52826e318a6effecc36fe2b2a5af
dxlpi <hjanggeog9@gmail.com>
2025-12-04 22:16:54 +0900

    Simplify breeding count and combo represenatation

--- src/libserver/data/file/FileDataSource.cpp
+++ src/libserver/data/file/FileDataSource.cpp
@@ -616,8 +616,8 @@ void server::FileDataSource::RetrieveHorse(data::Uid uid, data::Horse& horse)
   horse.fatigue = json.value("fatigue", uint16_t{0});
 
   const auto& breedingJson = json["breeding"];
-  horse.breeding.breedingCount = breedingJson.value("breedingCount", uint32_t{0});
-  horse.breeding.breedingCombo = breedingJson.value("breedingCombo", uint32_t{0});
+  horse.breeding.breedingCount = breedingJson.value("count", uint32_t{0});
+  horse.breeding.breedingCombo = breedingJson.value("combo", uint32_t{0});
 
   horse.type = json.value("type", uint32_t{0});
   horse.tendency = json.value("tendency", uint32_t{0});
@@ -733,8 +733,8 @@ void server::FileDataSource::StoreHorse(data::Uid uid, const data::Horse& horse)
   json["grade"] = horse.grade();
   json["growthPoints"] = horse.growthPoints();
   
-  json["breeding"]["breedingCount"] = horse.breeding.breedingCount();
-  json["breeding"]["breedingCombo"] = horse.breeding.breedingCombo();
+  json["breeding"]["count"] = horse.breeding.breedingCount();
+  json["breeding"]["combo"] = horse.breeding.breedingCombo();
 
   json["breeding"]["breedingCount"] = horse.breeding.breedingCount();
   json["breeding"]["breedingCombo"] = horse.breeding.breedingCombo();
