dc4665e0cf39ed304132e14609798e7da34ad5e7
Koaio <110025561+Koaio@users.noreply.github.com>
2026-03-09 14:16:31 +0400

    Remove ConnectionThrottle class, inline into Server
    Moved constants, IpState struct, and data members into Server's private section. Removed _throttle member. Added missing deque and chrono includes

--- include/libserver/network/Server.hpp
+++ include/libserver/network/Server.hpp
@@ -17,184 +17,181 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  **/
 
-#ifndef SERVER_HPP
-#define SERVER_HPP
+ #ifndef SERVER_HPP
+ #define SERVER_HPP
+ 
+ #include "NetworkDefinitions.hpp"
+ 
+ #include <functional>
+ #include <unordered_map>
+ #include <span>
+ #include <queue>
+ #include <deque>
+ #include <mutex>
+ #include <chrono>
+ 
+ #include <boost/asio.hpp>
+ 
+ namespace server::network
+ {
+ 
+ namespace asio = boost::asio;
+ 
+ //! A write handler.
+ using WriteSupplier = std::function<size_t(asio::streambuf&)>;
+ 
+ //!
+ class EventHandlerInterface
+ {
+ public:
+   virtual ~EventHandlerInterface() = default;
+ 
+   //! Handler of a network tick.
+   virtual void HandleNetworkTick() = 0;
+ 
+   //! Handler of client connection event.
+   //! @param clientId ID of the client connected.
+   virtual void OnClientConnected(ClientId clientId) = 0;
+ 
+   //! Handler of client disconnection event.
+   //! @param clientId ID of the client disconnected.
+   virtual void OnClientDisconnected(ClientId clientId) = 0;
+ 
+   //! Handler of client data event.
+   //! @param clientId ID of the client that sent the data.
+   //! @param data Byte buffer of the data sent.
+   //! @returns Count of bytes consumed from the byte buffer.
+   virtual size_t OnClientData(
+     ClientId clientId,
+     const std::span<const std::byte>& data) = 0;
+ };
+ 
+ //! Client with event driven reads and writes
+ //! to the underlying socket connection.
+ class Client : public std::enable_shared_from_this<Client>
+ {
+ public:
+   //! Default constructor.
+   //! @param socket Underlying socket.
+   explicit Client(
+     ClientId clientId,
+     asio::ip::tcp::socket&& socket,
+     EventHandlerInterface& networkEventHandler) noexcept;
+ 
+   //! Begins the client's asynchronous read loop.
+   void Begin();
+   //! Ends the client's asynchronous read loop.
+   void End();
+   //! Queues a write.
+   void QueueWrite(WriteSupplier writeSupplier);
+   //!
+   asio::ip::address_v4 GetAddress();
+ 
+ private:
+   void WriteLoop() noexcept;
+   //! Read loop.
+   void ReadLoop() noexcept;
+ 
+   //! Indicates whether the client should process I/O.
+   std::atomic<bool> _shouldRun = false;
+ 
+   //! A mutex for write buffer.
+   std::mutex _writeMutex;
+   //! A queue of write suppliers.
+   std::queue<WriteSupplier> _writeQueue{};
+   std::condition_variable _writeCv{};
+   //! A write buffer.
+   asio::streambuf _writeBuffer{};
+   std::atomic<bool> _isSending = false;
+ 
+   //! A read buffer.
+   asio::streambuf _readBuffer{};
+ 
+   //! A unique-identifier of the client.
+   ClientId _clientId;
+   //! A client socket.
+   asio::ip::tcp::socket _socket;
+   //! A network event handling interface
+   EventHandlerInterface& _networkEventHandler;
+ };
+ 
+ //! Server with event-driven acceptor, reads and writes.
+ class Server :
+   public EventHandlerInterface
+ {
+ public:
+   //! Default constructor.
+   explicit Server(
+     EventHandlerInterface& networkEventHandler) noexcept;
 
-#include "NetworkDefinitions.hpp"
+   //! Begins the server on the current thread.
+   //! Blocks the current thread until stopped.
+   //!
+   //! @param address Address of the interface to bind to.
+   //! @param port Port to bind to.
+   //! @throw std::runtime_error
+   void Begin(
+     const asio::ip::address& address,
+     uint16_t port);
 
-#include <functional>
-#include <unordered_map>
-#include <span>
-#include <queue>
-#include <mutex>
+   //! Ends the server.
+   void End();
 
-#include <boost/asio.hpp>
+   //! Get client.
+   std::shared_ptr<Client> GetClient(ClientId clientId);
 
-namespace server::network
-{
+   void HandleNetworkTick() override;
+   void OnClientConnected(ClientId clientId) override;
+   void OnClientDisconnected(ClientId clientId) override;
+   size_t OnClientData(ClientId clientId, const std::span<const std::byte>& data) override;
 
-namespace asio = boost::asio;
+ private:
+   void AcceptLoop() noexcept;
+   void TickLoop() noexcept;
 
-//! A write handler.
-using WriteSupplier = std::function<size_t(asio::streambuf&)>;
+   //! Checks whether a new connection from the given address should be accepted.
+   //! If accepted, records the connection internally.
+   //! @param address The remote IPv4 address.
+   //! @returns true if the connection is allowed.
+   bool AllowConnection(const asio::ip::address_v4& address);
 
-//!
-class EventHandlerInterface
-{
-public:
-  virtual ~EventHandlerInterface() = default;
+   //! Records that a client from the given address has disconnected.
+   //! @param address The remote IPv4 address.
+   void OnThrottleDisconnect(const asio::ip::address_v4& address);
 
-  //! Handler of a network tick.
-  virtual void HandleNetworkTick() = 0;
+   //! Maximum number of simultaneous connections from a single IP.
+   static constexpr size_t MaxConnectionsPerIp = 3;
+   //! Maximum total number of connected clients.
+   static constexpr size_t MaxTotalConnections = 200;
+   //! Maximum number of new connections from one IP within the sliding window.
+   static constexpr size_t MaxConnectRatePerIp = 10;
+   //! Duration of the sliding window for rate limiting.
+   static constexpr auto RateWindow = std::chrono::seconds(30);
 
-  //! Handler of client connection event.
-  //! @param clientId ID of the client connected.
-  virtual void OnClientConnected(ClientId clientId) = 0;
+   struct IpState
+   {
+     size_t activeConnections{0};
+     std::deque<std::chrono::steady_clock::time_point> connectionTimestamps;
+   };
 
-  //! Handler of client disconnection event.
-  //! @param clientId ID of the client disconnected.
-  virtual void OnClientDisconnected(ClientId clientId) = 0;
+   std::mutex _throttleMutex;
+   std::unordered_map<uint32_t, IpState> _ipStates;
 
-  //! Handler of client data event.
-  //! @param clientId ID of the client that sent the data.
-  //! @param data Byte buffer of the data sent.
-  //! @returns Count of bytes consumed from the byte buffer.
-  virtual size_t OnClientData(
-    ClientId clientId,
-    const std::span<const std::byte>& data) = 0;
-};
+   asio::io_context _io_ctx;
+   asio::ip::tcp::acceptor _acceptor;
+   asio::steady_timer _timer;
 
-//! Client with event driven reads and writes
-//! to the underlying socket connection.
-class Client : public std::enable_shared_from_this<Client>
-{
-public:
-  //! Default constructor.
-  //! @param socket Underlying socket.
-  explicit Client(
-    ClientId clientId,
-    asio::ip::tcp::socket&& socket,
-    EventHandlerInterface& networkEventHandler) noexcept;
+   //! Sequential client ID.
+   ClientId _client_id = 0;
+   //! Map of clients.
+   std::unordered_map<ClientId, std::shared_ptr<Client>> _clients;
+   //! Maps client ID to its remote address for disconnect tracking.
+   std::unordered_map<ClientId, asio::ip::address_v4> _clientAddresses;
 
-  //! Begins the client's asynchronous read loop.
-  void Begin();
-  //! Ends the client's asynchronous read loop.
-  void End();
-  //! Queues a write.
-  void QueueWrite(WriteSupplier writeSupplier);
-  //!
-  asio::ip::address_v4 GetAddress();
-
-private:
-  void WriteLoop() noexcept;
-  //! Read loop.
-  void ReadLoop() noexcept;
-
-  //! Indicates whether the client should process I/O.
-  std::atomic<bool> _shouldRun = false;
-
-  //! A mutex for write buffer.
-  std::mutex _writeMutex;
-  //! A queue of write suppliers.
-  std::queue<WriteSupplier> _writeQueue{};
-  std::condition_variable _writeCv{};
-  //! A write buffer.
-  asio::streambuf _writeBuffer{};
-  std::atomic<bool> _isSending = false;
-
-  //! A read buffer.
-  asio::streambuf _readBuffer{};
-
-  //! A unique-identifier of the client.
-  ClientId _clientId;
-  //! A client socket.
-  asio::ip::tcp::socket _socket;
-  //! A network event handling interface
-  EventHandlerInterface& _networkEventHandler;
-};
-
-class ConnectionThrottle {
-
-public:
-  //! Maximum number of simultaneous connections from a single IP
-  static constexpr size_t MaxConnectionsPerIp = 3;
-  //! Maximum total number of connected clients
-  static constexpr size_t MaxTotalConnections = 200;
-  //! Maximum number of new connections from one IP within the sliding window
-  static constexpr size_t MaxConnectRatePerIp = 10;
-  //! Duration of the sliding window for rate limiting
-  static constexpr auto RateWindow = std::chrono::seconds(30);
-
-  //! Checks whether a new connection from the given address should be accepted
-  //! If accepted, records the connection internally
-  //! @param address The remote IPv4 address
-  //! @param totalConnections Current total number of connected clients
-  //! @returns true if the connection is allowed
-  bool AllowConnection(const asio::ip::address_v4& address, size_t totalConnections):
-
-  //! Records that a client from the given address has disconnected
-  //! @param address The remote IPv4 address
-  void OnDisconnect(const asio::ip::address_v4& address);
-
-private:
-  struct IpState { size_t activeConnections{0}; std::deque<std::chrono::steady_clock::time_point> connectionTimestamps; };
-
-  std::mutex _mutex;
-  std::unordered_map<uint32_t, IpState> _ipStates;
-}
-
-//! Server with event-driven acceptor, reads and writes.
-class Server :
-  public EventHandlerInterface
-{
-public:
-  //! Default constructor.
-  explicit Server(
-    EventHandlerInterface& networkEventHandler) noexcept;
-
-  //! Begins the server on the current thread.
-  //! Blocks the current thread until stopped.
-  //!
-  //! @param address Address of the interface to bind to.
-  //! @param port Port to bind to.
-  //! @throw std::runtime_error
-  void Begin(
-    const asio::ip::address& address,
-    uint16_t port);
-
-  //! Ends the server.
-  void End();
-
-  //! Get client.
-  std::shared_ptr<Client> GetClient(ClientId clientId);
-
-  void HandleNetworkTick() override;
-  void OnClientConnected(ClientId clientId) override;
-  void OnClientDisconnected(ClientId clientId) override;
-  size_t OnClientData(ClientId clientId, const std::span<const std::byte>& data) override;
-
-private:
-  void AcceptLoop() noexcept;
-  void TickLoop() noexcept;
-
-  asio::io_context _io_ctx;
-  asio::ip::tcp::acceptor _acceptor;
-  asio::steady_timer _timer;
-
-  //! Sequential client ID.
-  ClientId _client_id = 0;
-  //! Map of clients.
-  std::unordered_map<ClientId, std::shared_ptr<Client>> _clients;
-  //! Maps client ID to its remote address for disconnect tracking
-  std::unordered_map<ClientId, asio::ip::address_v4> _clientAddresses;
-
-  //! Connection throttle.
-  ConnectionThrottle _throttle;   
-
-  //! A network event handler.
-  EventHandlerInterface& _networkEventHandler;
-};
-
-} // namespace server
-
-#endif // SERVER_HPP
+   //! A network event handler.
+   EventHandlerInterface& _networkEventHandler;
+ };
+ 
+ } // namespace server
+ 
+ #endif // SERVER_HPP
