8547961e421b39d8c57129c93dc3bf693b692510
Xenii <xeniiakukushkina@gmail.com>
2026-03-10 14:52:40 +0100

    fix item display

--- src/server/ranch/RanchDirector.cpp
+++ src/server/ranch/RanchDirector.cpp
@@ -5250,6 +5250,12 @@ void RanchDirector::HandleRequestDailyQuestReward(
 
       response.rewards.items.emplace_back(
         protocol::Item{.uid = itemUid, .tid = rewardItem.tid, .expiresAt = 0, .count = rewardItem.count});
+      const auto itemRecord = _serverInstance.GetDataDirector().GetItem(itemUid);
+
+        {
+          auto& protocolItem = response.rewards.items.emplace_back();
+          protocol::BuildProtocolItem(protocolItem, item);
+        });      
     }
   });
 
@@ -5263,10 +5269,10 @@ void RanchDirector::HandleRequestDailyQuestReward(
   protocol::AcCmdRCUpdateDailyQuestNotify noti{
     .characterUid = clientContext.characterUid,
     .questId = command.questTid,
-    .unk = {.isCompleted = 1, .progress = 0, .unk2 = 0},
+    .unk = {.isCompleted = 1, .progress = 0, .unk2 = 1},
     .carrotsReward = 0,
     .questType = 0,
-    .unk2 = 0,
+    .unk2 = 1000,
     .mountExp = 0};
 
   _commandServer.QueueCommand<decltype(noti)>(
