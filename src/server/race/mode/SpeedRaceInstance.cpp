3e10f41b42b52c5f567a69c410176b1c4ba7b6a7
Serkan <14278530+SergeantSerk@users.noreply.github.com>
2026-04-17 18:05:30 +0100

    Scaffold gamemode instances

---
+++ src/server/race/mode/SpeedRaceInstance.cpp
@@ -0,0 +1,36 @@
+/**
+ * Alicia Server - dedicated server software
+ * Copyright (C) 2026 Story Of Alicia
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ **/
+
+#include "server/ServerInstance.hpp"
+
+#include "server/race/mode/SpeedRaceInstance.hpp"
+
+#include <spdlog/spdlog.h>
+
+namespace server
+{
+
+SpeedRaceInstance::SpeedRaceInstance(
+  ServerInstance& serverInstance,
+  CommandServer& commandServer) : RaceInstance(serverInstance, commandServer)
+{}
+
+SpeedRaceInstance::~SpeedRaceInstance() = default;
+
+} // namespace server
