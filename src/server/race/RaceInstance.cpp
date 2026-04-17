eab81f4875c1a8c75863d35bddc69c7b162003a5
Serkan <14278530+SergeantSerk@users.noreply.github.com>
2026-04-17 16:46:41 +0100

    Extract race instance loading stage

--- src/server/race/RaceInstance.cpp
+++ src/server/race/RaceInstance.cpp
@@ -17,19 +17,91 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  **/
 
+#include "server/ServerInstance.hpp"
+
 #include "server/race/RaceInstance.hpp"
 
 #include <spdlog/spdlog.h>
 
+#include <ranges>
+
 namespace server
 {
 
-RaceInstance::RaceInstance() = default;
+RaceInstance::RaceInstance(
+  ServerInstance& serverInstance,
+  CommandServer& commandServer) : _serverInstance(serverInstance), _commandServer(commandServer)
+{}
 RaceInstance::~RaceInstance() = default;
+
+//! Converts a steady clock's time point to a race clock's time point.
+//! @param timePoint Time point.
+//! @return Race clock time point.
+uint64_t RaceInstance::TimePointToRaceTimePoint(const std::chrono::steady_clock::time_point& timePoint)
+{
+  // Amount of 100ns
+  constexpr uint64_t IntervalConstant = 100;
+  return std::chrono::duration_cast<std::chrono::nanoseconds>(
+    timePoint.time_since_epoch()).count() / IntervalConstant;
+}
 
 void RaceInstance::TickLoading()
 {
+  // Determine whether all racers have started racing.
+  const bool allRacersLoaded = std::ranges::all_of(
+    std::views::values(tracker.GetRacers()),
+    [](const tracker::RaceTracker::Racer& racer)
+    {
+      return racer.state == tracker::RaceTracker::Racer::State::Racing
+        || racer.state == tracker::RaceTracker::Racer::State::Disconnected;
+    });
 
+  const bool loadTimeoutReached = std::chrono::steady_clock::now() >= stageTimeoutTimePoint;
+
+  // If not all the racers have loaded yet and the timeout has not been reached yet
+  // do not start the race.
+  if (not allRacersLoaded && not loadTimeoutReached)
+    return;
+
+  if (loadTimeoutReached)
+  {
+    spdlog::warn("Room {} has reached the loading timeout threshold", roomUid);
+  }
+
+  for (auto& racer : tracker.GetRacers() | std::views::values)
+  {
+    // todo: handle the players that did not load in to the race.
+    // for now just consider them disconnected
+    if (racer.state != tracker::RaceTracker::Racer::State::Racing)
+      racer.state = tracker::RaceTracker::Racer::State::Disconnected;
+  }
+
+  const auto mapBlockTemplate = _serverInstance.GetCourseRegistry().GetMapBlockInfo(
+    raceMapBlockId);
+
+  // Switch to the racing stage and set the timeout time point.
+  stage = RaceInstance::Stage::Racing;
+  stageTimeoutTimePoint = std::chrono::steady_clock::now() + std::chrono::seconds(
+    mapBlockTemplate.timeLimit);
+
+  // Set up the race start time point.
+  const auto now = std::chrono::steady_clock::now();
+  raceStartTimePoint = now + std::chrono::seconds(
+    mapBlockTemplate.waitTime);
+
+  // Broadcast the race countdown.
+  const protocol::AcCmdUserRaceCountdown raceCountdown{
+    .raceStartTimestamp = TimePointToRaceTimePoint(raceStartTimePoint)};
+
+  for (const network::ClientId clientId : clients)
+  {
+    _commandServer.QueueCommand<protocol::AcCmdUserRaceCountdown>(
+      clientId,
+      [&raceCountdown]()
+      {
+        return raceCountdown;
+      });
+  }
 }
 
 void RaceInstance::TickRacing()
@@ -44,7 +116,24 @@ void RaceInstance::TickFinishing()
 
 void RaceInstance::Tick()
 {
-
+  // Tick instance based on which stage it is on
+  switch(this->stage)
+  {
+    case RaceInstance::Stage::Waiting:
+      // Do nothing, room is not racing
+      break;
+    case RaceInstance::Stage::Loading:
+      TickLoading();
+      break;
+    case RaceInstance::Stage::Racing:
+      TickRacing();
+      break;
+    case RaceInstance::Stage::Finishing:
+      TickFinishing();
+      break;
+    default:
+      throw std::runtime_error("Race instance stage is not recognised");
+  }
 }
 
 } // namespace server
